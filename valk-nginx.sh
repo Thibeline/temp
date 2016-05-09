19fd5d38fc6673a5f0c3649fcff7900d06ab40cb
thibaud Chatrefoux <thibaud.chatrefoux@gmail.com>
2016-05-09 15:47:15 +0200

    Function disabled working

--- valk-nginx.sh
+++ valk-nginx.sh
@@ -18,23 +18,14 @@ domain_name=$1
 ############# FUNCTION ###############
 ######################################
 
-# Check if the domain name exist. If he exists return 0 else return 1.
-function check_domain_name {
 
-
-	if [ -f "/etc/nginx/conf.d/""$domain_name"* ];then
-		return 0
-	else
-		return 1
-	fi
-
-}
 
 # Disabled the domain enter in argument and reload nginx to applicate this modification
-function disabled { 
-	i=check_domain_name domain_name
+# OK
 
-	if [ i=1 ] then
+function disabled { 
+
+	if [ ! -e "/etc/nginx/conf.d/""$domain_name"* ]; then
 		echo " This domain doesn't exist"
 		exit 10 
 	fi
