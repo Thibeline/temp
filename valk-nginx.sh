0f604e81c183d0a924d56b65b6fda4eec133e20c
Thibeline <thibaud.chatrefoux@gmail.com>
2016-05-09 16:57:55 +0200

    Update valk-nginx.sh

--- valk-nginx.sh
+++ valk-nginx.sh
@@ -29,15 +29,31 @@ domain_name=$1
 
 function disabled { 
 
-	if [ ! -e "/etc/nginx/conf.d/""$domain_name"".conf" ]; then
-		echo " file /etc/nginx/conf.d/""$domain_name"".conf not found" 
-		exit 10
-
+	if [ ! -e "/etc/nginx/conf.d/""$domain_name"* ]; then
+		echo " This domain doesn't exist"
+		exit 10 
 	fi
 
-mv /etc/nginx/conf.d/"$domain_name".conf /etc/nginx/conf.d/"$domain_name".disabled
+	mv /etc/nginx/conf.d/"$domain_name".conf /etc/nginx/conf.d/"$domain_name".disabled
 
 	systemctl reload nginx
+
+}
+
+# Enabled the domain enter in argument and reload nginx to applicate this modification
+#OK
+function enabled {
+	
+	if [ ! -e "/etc/nginx/conf.d/""$domain_name"".disabled" ]; then
+		echo " File /etc/nginx/conf.d/""$domain_name"".disable not found"
+		exit 10 
+	fi
+
+	mv /etc/nginx/conf.d/"$domain_name".disabled /etc/nginx/conf.d/"$domain_name".conf
+
+	systemctl reload nginx
+
+}
 
 ######################################
 ############# PROTECTED ##############
